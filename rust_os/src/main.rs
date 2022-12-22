bb3a229f12b419ae32f0b5e832438350e220d1cb
twist9743 <twist9743@mail.ru>
2022-12-22 13:01:33 +0300

    Fix interrupts in cargo
    Signed-off-by: twist9743 <twist9743@mail.ru>

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -36,8 +36,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
         println!("{:?} -> {:?}", virt, phys);
     }
 */
-        x86_64::instructions::interrupts::int3();
-
+/*
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = memory::EmptyFrameAllocator;
@@ -51,9 +50,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // write the string `New!` to the screen through the new mapping
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+*/
 
-        x86_64::instructions::interrupts::int3();
-
+	x86_64::instructions::interrupts::int3();
     println!("It did not crash!");
 
     // Тестовый запуск
